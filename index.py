e39b1f8263813bcdb77edb0ddb77d63d4a5184b5
SHEMNDARONGUGI <ndaroshem@gmail.com>
2025-03-04 14:38:21 +0300

    Inital commit

--- index.py
+++ index.py
@@ -1,2 +1,5 @@
 print("Hello world")
-x = 1
+x = 1 
+y = 2
+z = x + y
+print(z)
---
+++ lesson1.py
@@ -0,0 +1,26 @@
+num1 = float(input("Enter the first number: "))
+num2 = float(input("Enter the second number: "))
+
+# sum of the two numbers
+sum_result = num1 + num2
+
+# product of the two numbers
+product_result = num1 * num2
+
+# difference of the two numbers
+difference_result = num1 - num2
+
+# quotient of the two numbers
+quotient_result = num1 / num2
+
+# Printing out the results
+
+print(f"Results of your two numbers:")
+print(f"Sum: {sum_result}")
+print(f"Difference: {difference_result}")  
+print(f"Product: {product_result}")  
+print(f"Quotient: {quotient_result}")  
+ 
+
+
+
